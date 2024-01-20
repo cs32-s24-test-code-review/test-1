06e99d6ab087f159a488607d5bb4aa58b6ec794a
Michelle Ding <michelle_ding@brown.edu>
2024-01-20 11:26:35 -0500

    random python files test & test1

---
+++ test1.py
@@ -0,0 +1,9 @@
+def reverse_string(input_string):
+    reversed_string = input_string[::-1]
+    return reversed_string
+
+original_string = "Hello, World!"
+reversed_result = reverse_string(original_string)
+
+print("Original String:", original_string)
+print("Reversed String:", reversed_result)
